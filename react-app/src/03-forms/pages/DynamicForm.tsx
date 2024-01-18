28a4976bf69b172e04fcdeeeee8b174b7cbc7c78
Kevin Sarria <camilo.bravo.2050@gmail.com>
2024-01-18 16:11:16 -0500

    Formularios dinamicos

--- react-app/src/03-forms/pages/DynamicForm.tsx
+++ react-app/src/03-forms/pages/DynamicForm.tsx
@@ -1,7 +1,52 @@
 import { Formik, Form } from 'formik';
 
+import * as Yup from 'yup';
+
 import formJson from '../data/custom-form.json';
-import { MyTextInput } from '../components';
+import { MySelect, MyTextInput } from '../components';
+
+interface InitialValues {
+    [key: string]: any;
+}
+
+interface RequiredFields {
+    [key: string]: any;
+}
+
+const initialValues: InitialValues = {};
+const requiredFields: RequiredFields = {};
+
+for (const input of formJson) {
+    initialValues[input.name] = input.value;
+
+    if( !input.validations ) continue;
+
+    let schema = Yup.string();
+
+    for (const rule of input.validations) {
+
+        if( rule.type === 'required' ) {
+            schema = schema.required('Este Campo es Requerido');
+        }
+
+        // otras reglas...
+
+        if( rule.type === "minLength" ) {
+            schema = schema.min( (rule as any).value || 2, `Minimo de ${ (rule as any).value || 2 } caracteres`);
+        }
+
+        if( rule.type === "email" ) {
+            schema = schema.email('Email no Valido');
+        }
+
+    }
+
+    requiredFields[input.name] = schema;
+
+}
+
+const validationSchema = Yup.object({ ...requiredFields });
+
 
 export const DynamicForm = () => {
 
@@ -10,28 +55,47 @@ export const DynamicForm = () => {
         <h1>Dynamic Form</h1>
 
         <Formik
-            initialValues={{
-                name: ''
-            }}
+            initialValues={initialValues}
+            validationSchema={validationSchema}
             onSubmit={(values) => {
                 console.log(values)
             }}
         >
             {
                 (formik) => (
-                    <Form>
+                    <Form noValidate>
                         
                         {
-                            formJson.map( ({ label, type, name, placeholder, value }, index) => (
-                                <MyTextInput
-                                    key={index}
-                                    type={type as any}
-                                    label={label}
-                                    name={name}
-                                    placeholder={placeholder}
-                                    value={value}
-                                />
-                            ) )
+                            formJson.map( ({ label, type, name, placeholder, options }, index) => {
+
+                                if( type === 'text' || type === 'password' || type === 'email' ) {
+                                    return (
+                                        <MyTextInput
+                                            key={index}
+                                            type={(type as any)}
+                                            label={label}
+                                            name={name}
+                                            placeholder={placeholder}
+                                        />
+                                    )
+                                } else if( type === 'select' ) {
+                                    return (
+                                        <MySelect
+                                            key={ index }
+                                            label={ label }
+                                            name={ name }
+                                        >
+                                            <option value="">Select an Option</option>
+                                            {
+                                                options?.map( ({ id, label }, index) => (
+                                                    <option key={index} value={id}>{label}</option>
+                                                ) )
+                                            }
+                                        </MySelect>
+                                    )
+                                }
+
+                            })
                         }
 
                         <button type='submit'>Guardar</button>
